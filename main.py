d8cfec34cad8a478f590cb74681251c436a03146
parimal5 <parimalmeshram15@gmail.com>
2021-08-03 23:36:32 +0530

    Added Key Presses TO Move the Snake using arrow

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from turtle import Turtle, Screen
+from snake import Snake
+
 import time
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(height=600, width=600)
 screen.title("My Snake Game")
 screen.tracer(0)
 
-pos = [(0, 0), (-20, 0), (-40, 0)]
-snakes = []
 
-for box in range(3):
-    new_box = Turtle("square")
-    new_box.color("white")
-    new_box.penup()
-    new_box.goto(pos[box])
-    snakes.append(new_box)
-
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 
 game_is_on = True
-
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for snake in snakes:
-        snake.forward(20)
+    snake.move()
 
 screen.exitonclick()
