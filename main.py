b06230492b1bce2ea1d36edf1f3c0e5eb7ba7083
parimal5 <parimalmeshram15@gmail.com>
2021-08-04 21:15:39 +0530

    Added Score When snake hit the food.

--- main.py
+++ main.py
@@ -29,6 +29,6 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
-
+        score.increase_score()
 
 screen.exitonclick()
--- score_board.py
+++ score_board.py
@@ -1,15 +1,24 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
 class Score(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.score = 0.
+        self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 27.60)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.goto(0, 260)
         self.hideturtle()
+        self.update_score()
 
-        
-    
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
